465ae89df4e71b02cc89a582ced7d6daddfd07da
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-11-25 16:42:26 +0100

    update tacks 1 and 2

--- src/2.ts
+++ src/2.ts
@@ -16,9 +16,9 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  public name: string;
+  private department: string;
+  protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -31,9 +31,10 @@ class Employee {
   }
 }
 
-class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+class Manager1 extends Employee {
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  } // Реалізуйте конструктор та збільшіть salary на 10000
 }
-
 
 export {};
